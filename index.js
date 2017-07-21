91a41adee61f7a609b1905c5d76a5580c6718457
Alan Jenkins <alan@functionfirst.co.uk>
2017-07-21 14:16:11 +0100

    Added endpoint to show contacts list

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ var express = require('express');
   config = require('./config'),
   port = process.env.PORT || config.port || 3000;
 
+var contacts = [{
+  name: 'Alan',
+  email: 'foo@bar.com'
+}];
+
+app.get('/phonebook', function(req, res) {
+  var contacts = [{
+    name: 'Alan',
+    email: 'foo@bar.com'
+  }];
+
+  res.json(contacts);
+});
+
 app.get('*', function(req, res) {
   res.json({
     message: 'Welcome to the Phonebook API'
